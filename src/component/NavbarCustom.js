f237254d45fa6a960110e278cc2894d343e6045d
Gwenaëlle Cadic <gwenaelle.cadic@infrasoft.be>
2022-03-20 14:12:46 +0100

    horizontal subMenu.

--- src/component/NavbarCustom.js
+++ src/component/NavbarCustom.js
@@ -1,35 +1,64 @@
 import React from "react";
-import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Container, Navbar, Nav } from "react-bootstrap";
+
+const navigationItems = {
+    Cabinet: ["Presentation", "Equipe", "Missions"],
+    Formation: ["Catalogue", "Sur mesure"]
+};
 
 class NavbarCustom extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            hasSubMenu: false,
+            activeSubMenu: "",
+        };
+        this.setHasSubMenu = this.setHasSubMenu.bind(this);
 
+    }
+    setHasSubMenu(newActiveSubMenu) {
+        this.setState({ activeSubMenu: this.state.hasSubMenu && (this.state.activeSubMenu === newActiveSubMenu)? "" : newActiveSubMenu, hasSubMenu: this.state.hasSubMenu && (this.state.activeSubMenu !== newActiveSubMenu)? true : !this.state.hasSubMenu });
+    }
     render() {
-        return(
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand href="/">PLC</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
+        return (
+            <>
+                <Navbar bg="light" expand="lg">
+                    <Container>
+                        <Navbar.Brand href="/">PLC</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        {/* <Navbar.Collapse id="basic-navbar-nav"> */}
+                            <Nav className="me-auto">
+                                <Nav.Link onClick={() => this.setHasSubMenu("Cabinet")}>Cabinet</Nav.Link>
+                                <Nav.Link onClick={() => this.setHasSubMenu("Formation")}>Formation</Nav.Link>
+                                {/*    <NavDropdown title="" id="cabinet-dropdown">
+                                    <NavDropdown.Item href="#action/3.1">Présentation</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.2">Equipe</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3">Missions</NavDropdown.Item>
+                                </NavDropdown>
+                                <NavDropdown title="" id="formation-dropdown">
+                                    <NavDropdown.Item href="/formationsCatalogue">Catalogue</NavDropdown.Item>
+                                    <NavDropdown.Item href="/customFormation">Sur mesure</NavDropdown.Item>
+                                </NavDropdown>*/}
+                            </Nav>
+                            <Nav>
+                                <Nav.Link href="#practical-info">Infos pratiques</Nav.Link>
+                                <Nav.Link href="#contact">Contact</Nav.Link>
+                            </Nav>
+                        {/* </Navbar.Collapse> */}
+                    </Container>
+                </Navbar>
+                {this.state.hasSubMenu &&
                     <Nav className="me-auto">
-                        <NavDropdown title="Cabinet" id="cabinet-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Présentation</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Equipe</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Missions</NavDropdown.Item>
-                        </NavDropdown>
-                        <NavDropdown title="Formation" id="formation-dropdown">
-                        <NavDropdown.Item href="/formationsCatalogue">Catalogue</NavDropdown.Item>
-                        <NavDropdown.Item href="/customFormation">Sur mesure</NavDropdown.Item>
-                        </NavDropdown>
+                        {
+                            navigationItems[this.state.activeSubMenu].map((item, index) => {
+                                return <Nav.Link href={`#${item.toLowerCase()}`} key={index + item}>{item}</Nav.Link>
+                            })
+                        }
                     </Nav>
-                    <Nav>
-                        <Nav.Link href="#practical-info">Infos pratiques</Nav.Link>
-                        <Nav.Link href="#contact">Contact</Nav.Link>
-                    </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+                }
+            </>
         )
-        
+
     }
 
 }
