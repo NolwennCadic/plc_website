c82439d62ee2d19c1bd64b6689638c3c8f7da246
Gwenaëlle Cadic <gwenaelle.cadic@infrasoft.be>
2022-05-22 16:31:37 +0200

    Improved FormationCatalog.

--- src/component/FormationCard.js
+++ src/component/FormationCard.js
@@ -1,38 +1,32 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import comptaImage from "../images/compta.jpeg";
+import formation1 from "../pdf/formation1.pdf";
+import formation2 from "../pdf/formation2.pdf";
 
+const fomationPdfs = {
+    1: formation1,
+    2: formation2,
+};
+
+const images = {
+    compta: comptaImage,
+}
+
+//Input, wa have formation data
 class FormationCard extends React.Component {
     render() {
-        // function downloadFile(url, name = "myFile.pdf") {
-        //     fetch(url).then(pdf => {
-        //         console.log("pdf =", pdf);
-        //         const blob = new Blob([pdf], { type: "application/pdf" });
-        //         const href = URL.createObjectURL(blob);
-
-        //         const a = Object.assign(document.createElement('a'), { href, style: "display:none", download: name });
-        //         document.body.appendChild(a);
-        //         a.click();
-        //         URL.revokeObjectURL(href);
-        //         a.remove()
-        //     })
-        // }
-
         return (
             <Card border="info" style={{ width: '18rem', border: "none" }}>
-                <Card.Img variant="top" src={comptaImage} style={{ padding: "10%", borderRadius: "0.5em" }} />
+                <Card.Img variant="top" src={images[this.props.formation.image]} style={{ padding: "10%", borderRadius: "0.5em" }} />
                 <hr class="my-12" />
                 <Card.Body>
 
-                    <Card.Title>Formation comptabilité niveau 1</Card.Title>
+                    <Card.Title>{this.props.formation.name}</Card.Title>
                     <Card.Text>
-                        Apprendre les bases de la comptabilité
+                        {this.props.formation.shortDescription}
                     </Card.Text>
-                    {/* <Button href="/formationDescription">En savoir plus</Button> */}
-                    {/* <a href="../pdf/formation1.pdf" onClick={() => downloadFile("C:/Users/gwenaellecadic/OneDrive - Infrasoft NV/Documents/Perso/Project/plc_website/src/pdf/formation1.pdf", "formation1.pdf")}>
-                        Download File
-                    </a> */}
-                    <a href="http://localhost:3000/src/pdf/formation1.pdf" download>Download File</a>
+                    <a href={fomationPdfs[this.props.formation.id]} download>Infomations supplémentaires</a>
                 </Card.Body>
             </Card>
         )
--- src/component/FormationsCatalogue.js
+++ src/component/FormationsCatalogue.js
@@ -1,11 +1,16 @@
 import React from "react";
 import FormationCard from "./FormationCard";
-
+import formationsData from "../data/formationDetails2.json"
 class FormationsCatalog extends React.Component {
 
     render() {
         return (
-            <FormationCard />
+            <div style={{display:"flex", flexDirection: "row", justifyContent: "center", gap: "10px"}}>
+                {formationsData.map((formation) => {
+                    return <FormationCard formation={formation} />
+                })
+                }
+            </div>
         );
     }
 
